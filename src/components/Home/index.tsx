ccf751decb1d4228134c8652ae99c427f00036b3
UlanTursunbek <ulantursunbekdev@gmail.com>
2023-03-14 12:18:53 +0600

    feat: Render list of repos

--- src/components/Home/index.tsx
+++ src/components/Home/index.tsx
@@ -1,5 +1,37 @@
+import { InfiniteLoadingTrigger } from "../../lib/InfiniteLoadingTrigger";
+import { useRepos } from "../../lib/useRepos";
+
 import classes from "./index.module.css";
+import { Item } from "../Item";
 
 export const Home = () => {
-  return <div>home</div>;
+  const { repos, isValidating, isLoading, isReady, loadMore, isReachedEnd } =
+    useRepos();
+
+  if (isLoading) {
+    return <div>Loading...</div>;
+  }
+
+  return (
+    <div className={classes.wrapper}>
+      {repos?.map((item) => (
+        <Item key={item.id} title={item.title} />
+      ))}
+
+      {isReady && (
+        <InfiniteLoadingTrigger
+          callbackLoadMore={loadMore}
+          isLoading={isValidating}
+          isReachedEnd={isReachedEnd}
+          ComponentButton={() => (
+            <button type="button" onClick={loadMore}>
+              Load more
+            </button>
+          )}
+          ComponentLoading={() => <span>Loading...</span>}
+          ComponentEmpty={() => <span>No more items</span>}
+        />
+      )}
+    </div>
+  );
 };
