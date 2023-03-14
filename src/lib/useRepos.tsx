0258ea6fd64f49a0ac78cfc1b46a2642a4447ea4
UlanTursunbek <ulantursunbekdev@gmail.com>
2023-03-14 12:20:13 +0600

    feat: Fetch data with useSWR

--- src/lib/useRepos.tsx
+++ src/lib/useRepos.tsx
@@ -1,6 +1,17 @@
 import useSWRInfinite from "swr/infinite";
+import { last } from "lodash";
 
-type ResponseBoby = {
+type CollectionOutput<T> = {
+  repos: T[];
+  isValidating: boolean;
+  isLoading: boolean;
+  loadMore: () => void;
+  isReachedEnd: boolean;
+  isReady: boolean;
+  isEmpty: boolean;
+};
+
+type ResponseBody = {
   id: number;
   title: string;
 };
@@ -10,18 +21,29 @@ const fetcher = (url: string) => fetch(url).then((response) => response.json());
 const PAGE_SIZE = 5;
 
 const getUrl = (pageIndex: number) =>
-  `https://api.github.com/repos/reactjs/react-a11y/issues?per_page=${PAGE_SIZE}&page=${
+  `https://api.github.com/repos/vercel/next.js/issues?per_page=${PAGE_SIZE}&page=${
     pageIndex + 1
   }`;
 
-const getKey = (pageIndex: number, previousPageData: ResponseBoby[]) => {
+const getKey = (pageIndex: number, previousPageData: ResponseBody[]) => {
   if (previousPageData && !previousPageData.length) return null; // reached the end
 
   return getUrl(pageIndex); // SWR key
 };
 
-export const useRepos = () => {
-  const { data } = useSWRInfinite(getKey, fetcher);
+export const useRepos = (): CollectionOutput<ResponseBody> => {
+  const { data, setSize, isValidating, isLoading, error } = useSWRInfinite(
+    getKey,
+    fetcher
+  );
 
-  return data;
+  return {
+    isReady: Boolean(data || error),
+    loadMore: () => setSize((size) => size + 1),
+    repos: data ? [].concat(...data) : [],
+    isReachedEnd: Boolean(error) || last(data)?.length === 0,
+    isEmpty: data?.length === 0,
+    isValidating,
+    isLoading,
+  };
 };
